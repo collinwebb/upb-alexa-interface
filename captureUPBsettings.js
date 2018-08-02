a6e035185ff0cabd8361a25de2de704a46ddd2cc
Collin Webb <collindwebb@gmail.com>
2018-08-02 15:17:17 -0700

    Clean up captureUPBsettings.js to remove useless scenes

--- captureUPBsettings.js
+++ captureUPBsettings.js
@@ -1,38 +1,50 @@
-let fs = require('fs'),
-    possibleFiles = fs.readdirSync('UPBsettings/sourceFile/'),
-    UPBfile = possibleFiles[0] === '.keep' ? possibleFiles[1] : possibleFiles[0];
+const fs = require('fs'),
+      possibleFiles = fs.readdirSync('UPBsettings/sourceFile/'),
+      UPBfile = possibleFiles[0] === '.keep' ? possibleFiles[1] : possibleFiles[0];
 
-let createUPBobjectEntries = (csvLine) => {
+// takes .csv or .upe lines and turns them into an array.
+const createUPBobjectEntries = (csvLine) => {
   let UPBarray = csvLine.split(',');
 
+  // in my .upe file, lines that start with '3' specify a switch and look like:
+  // 3,36,1,1,1,5,53,2,1,1,16,Kitchen,Lite over micro,1
+  // and may have varying numbers of entries between '3' and 'Kitchen'
   if (UPBarray[0] === '3') {
     let roomName = UPBarray[UPBarray.length - 3],
         lightName = UPBarray[UPBarray.length - 2],
         switchID = UPBarray[1];
 
-    return ['room', roomName, lightName, switchID];
-
+    return ['switch', roomName, lightName, switchID];
+  // lines that start with '2' specify scenes and look like:
+  // 2,23,Garage Off
   } else if (UPBarray[0] === '2') {
-    let scene = UPBarray[2],
+    let sceneName = UPBarray[2],
         sceneID = UPBarray[1];
 
-    return ['scene', scene, sceneID];
+    // ignore generic scene names
+    if (sceneName.substr(0, 5) === 'Scene') {
+      return ['nothing'];
+    }
+
+    return ['scene', sceneName, sceneID];
 
   } else {
     return ['nothing'];
   }
 };
 
-let processFile = (inputFile) => {
-  let readline = require('readline'),
+// highest level function to turn .upe files into .json structured object
+const processFile = (inputFile) => {
+  const readline = require('readline'),
       instream = fs.createReadStream(inputFile),
       outstream = new (require('stream'))(),
       rl = readline.createInterface(instream, outstream),
       UPBdictionary = {'rooms': {}, 'scenes': {}};
 
-  rl.on('line', function (line) {
-      newLineList = createUPBobjectEntries(line);
-      if (newLineList[0] === 'room') {
+  // For each line, make list and create an object from that list.
+  rl.on('line', (line) => {
+      let newLineList = createUPBobjectEntries(line);
+      if (newLineList[0] === 'switch') {
         if (typeof(UPBdictionary.rooms[newLineList[1]]) !== 'object') {
           UPBdictionary.rooms[newLineList[1]] = {'lights':{}};
         }
@@ -42,10 +54,11 @@ let processFile = (inputFile) => {
       }
   });
 
-  rl.on('close', function(){
-    fs.writeFile("UPBsettings/settings.json", JSON.stringify(UPBdictionary), function(err) {
+  // On Close write the file.
+  rl.on('close', () => {
+    fs.writeFile("UPBsettings/settings.json", JSON.stringify(UPBdictionary), (err) => {
       if(err) {
-        return console.log('error!!');
+        return console.log('error: ', err);
       }
     });
   });
