73a917bef3da0c62afadd2962b9c9f272e8b75e0
Collin Webb <collindwebb@gmail.com>
2018-08-02 18:14:25 -0700

    Created restrictions to prevent damage to non-dimmable switches

--- captureUPBsettings.js
+++ captureUPBsettings.js
@@ -56,6 +56,39 @@ const processFile = (inputFile) => {
 
   // On Close write the file.
   rl.on('close', () => {
+    // Adding some restrictions.
+    // [Note: if more restrictions are needed made a js file to handle them.]
+    UPBdictionary.switchOrSceneRestrictions = {
+      // The switchOrSceneID is the same as the object key.
+      // In this casse I needed to prevent the upb device ID 3 from being dimmed.
+
+      // I've made a value the same as the keys to more easily return the values...
+      //I don't actually know if this is a good idea, but I'm trying it.
+      '3': {
+        'switchOrSceneID': '3',
+        'commands': {
+          'activate': {'name': 'activate', 'commandNumber':20, 'allowed': true},
+          'deactivate': {'name': 'deactivate', 'commandNumber':21, 'allowed': true},
+          'goto': {'name': 'goto', 'commandNumber':22, 'allowed': false},
+          'fadeStart': {'name': 'fadeStart', 'commandNumber':23, 'allowed': false},
+          'fadeStop': {'name': 'fadeStop', 'commandNumber':24, 'allowed': false},
+          'blink': {'name': 'blink', 'commandNumber':25, 'allowed': false},
+          'indicate': {'name': 'indicate', 'commandNumber':26, 'allowed': false},
+          'toggle': {'name': 'toggle', 'commandNumber':27, 'allowed': true},
+          'reportState': {'name': 'reportState', 'commandNumber':30, 'allowed': true},
+          'storeState': {'name': 'storeState', 'commandNumber':31, 'allowed': true},
+          'ackResponse': {'name': 'ackResponse', 'commandNumber':80, 'allowed': true},
+          'setupTimeReport': {'name': 'setupTimeReport', 'commandNumber':85, 'allowed': true},
+          'deviceStateReport': {'name': 'deviceStateReport', 'commandNumber':86, 'allowed': true},
+          'deviceStatusReport': {'name': 'deviceStatusReport', 'commandNumber':87, 'allowed': true},
+          'registerValuesReport': {'name': 'registerValuesReport', 'commandNumber':90, 'allowed': true},
+          'RAMvaluesReport': {'name': 'RAMvaluesReport', 'commandNumber':91, 'allowed': true},
+          'rawDataReport': {'name': 'rawDataReport', 'commandNumber':92, 'allowed': true},
+          'heartbeatReport': {'name': 'heartbeatReport', 'commandNumber':93, 'allowed': true},
+          'deviceSignatureReport': {'name': 'deviceSignatureReport', 'commandNumber':143, 'allowed': true}
+        }
+      }
+    };
     fs.writeFile("UPBsettings/settings.json", JSON.stringify(UPBdictionary), (err) => {
       if(err) {
         return console.log('error: ', err);
